86e1baa3415a8592e7a78d3247e775f5d6f1b82d
Tony Zhang <tony546@msn.com>
2017-04-09 10:50:49 -0500

    prepare for 1.0.1

--- package.js
+++ package.js
@@ -1,6 +1,6 @@
 ﻿Package.describe({
   name: "tonystevens:datedropper",
-  version: "1.0.0",
+  version: "1.0.1",
   summary: "Fancy date picker for text input. A Meteor wrapper for Datedropper3 from Felice Gattuso",
   git: "https://github.com/tonystevens/meteor-datedropper",
 });
